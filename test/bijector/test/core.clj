2018da62bed7723ebde8455e29169bc4d129d360
Gary Fredericks <fredericksgary@gmail.com>
2011-07-05 20:50:47 -0400

    Added failing test for list function

--- test/bijector/test/core.clj
+++ test/bijector/test/core.clj
@@ -17,6 +17,22 @@
   (doseq [n nz]
     (is (= n (->> n (to t) (from t))))))
 
+(defn- type-has-elements
+  [t & els]
+  (doseq [el els]
+    (let [n (from t el)]
+      (is (integer? n))
+      (is (pos? n))
+      (is (= el (to t n))))))
+
 (deftest basic-types-test
   (doseq [t [NATURALS INTEGERS]]
     (test-a-type t)))
+
+(deftest list-of-test
+  (type-has-elements
+    (list-of INTEGERS)
+    [1 2 3]
+    []
+    [-1 -2 -1]
+    [500007]))
