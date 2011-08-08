0f72e226d60cf2e2d4b4ca1147d3f54d9a1b909e
Gary Fredericks <fredericksgary@gmail.com>
2011-08-08 18:41:35 -0500

    Set version to 0.0.1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject bijector "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject bijector "0.0.1"
+  :description "Bijections in Clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]],
   :main bijector.core)
